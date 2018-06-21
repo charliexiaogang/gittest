6031a52cd2fe2b9ec0678931dc082c9274da4823
charliexiaogang <charliexg@foxmail.com>
2018-06-21 15:31:15 +0800

    冲突测试

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        system();
+    }
+
+    private static void system(){
+        System.out.println("冲突测试");
     }
 }
